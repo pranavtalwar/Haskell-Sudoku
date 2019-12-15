56fb72e397a4d7edab2420589ffedbcdc7a21526
Pranav Talwar <talwar.pranav123@gmail.com>
2019-12-16 01:13:54 +0530

    added checking functions

--- Sudoku.hs
+++ Sudoku.hs
@@ -19,10 +19,25 @@ allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku empty
                 where empty = [[(Nothing, -1) | num2 <- [1..9]] | num <- [1..9]]
 
-
 constructPositionSudoku :: [String] -> [String] -> Sudoku
 constructPositionSudoku positions values = Sudoku [[(if (values !! rowNo !! colNo == '.') then Nothing else Just (digitToInt (values !! rowNo !! colNo)), (digitToInt char)) | (char, colNo) <- zip row [0..length row - 1]] | (row, rowNo) <- zip positions [0..length positions - 1]]
 
+
+checkSudokuOver :: Sudoku -> Bool
+checkSudokuOver (Sudoku a) = and [and [(if value == Nothing then False else True) | (value, position) <- row ] | row <- a]
+
+-- checkMove :: Sudoku -> Int -> Int -> Int -> Bool
+-- checkMove (Sudoku a) row col number = 
+
+checkRow :: [(Maybe Int, Int)] -> Int -> Bool
+checkRow row number = and [if (Just number) == value then False else True | (value, position) <- row]
+
+checkRow :: [(Maybe Int, Int)] -> Int -> Bool
+checkColumn column number = and [if (Just number) == value then False else True | (value, position) <- column]
+
+-- checkJigsawPiece :: Sudoku -> Int -> Int -> Int -> Bool
+-- checkJigsawPiece (Sudoku a) row column number = and [| row <- a]
+--     where piece = snd (a !! row !! column)
 
 
 
